dac5e9b3e4e97bec3d96eab6407a25b4e0b8a222
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-05-25 17:13:27 +0700

    update view dashboard

--- resources/views/templates-sidebar/layout.blade.php
+++ resources/views/templates-sidebar/layout.blade.php
@@ -64,6 +64,12 @@
         <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
                 <li>
+                    <a href="{{ route('home.index') }}"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        🏠
+                        <span class="ms-3"> Beranda</span>
+                    </a>
+                <li>
                     <a href="#"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
