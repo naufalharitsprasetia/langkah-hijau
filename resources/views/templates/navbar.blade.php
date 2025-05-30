9186084c84775d0165e6f0e349fb47296c8f0008
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-05-30 21:49:02 +0700

    rizky -> add tentang aplikasi

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -93,7 +93,7 @@
                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                     <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
-                    <a href="{{ route('post.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
+                    <a href="{{ route('home.tentang') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200"
                         role="menuitem" tabindex="-1" id="user-menu-item-0">Tentang Aplikasi</a>
                     <a href="{{ route('home.kontak') }}"
                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 hover:outline-none"
